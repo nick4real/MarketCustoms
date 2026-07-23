3ee08faffc26a2661d38a9ba73bbdb49d8e3874a
Nikolaj Kurkul <67540024+nick4real@users.noreply.github.com>
2026-07-23 14:52:28 +0200

    feat: enforce uniqueness of user id

--- Services/Profiles/MC.Profiles.Infrastructure/Persistence/AppRelationalDbContext.cs
+++ Services/Profiles/MC.Profiles.Infrastructure/Persistence/AppRelationalDbContext.cs
@@ -6,4 +6,13 @@ namespace WebStoreUser.Infrastructure.Persistence;
 public class AppRelationalDbContext(DbContextOptions<AppRelationalDbContext> options) : DbContext(options)
 {
     public DbSet<Profile> Profiles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Profile>()
+            .HasIndex(p => p.Auth0UserId)
+            .IsUnique();
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
