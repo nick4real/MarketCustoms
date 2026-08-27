25d663e8c6cc9e95541a45dd77fa81b032f98007
nick4real <67540024+nick4real@users.noreply.github.com>
2026-08-27 15:10:11 +0200

    style: prettier

--- Clients/MC.Market.WebApp/vite.config.ts
+++ Clients/MC.Market.WebApp/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
-import plugin from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, loadEnv } from "vite";
+import plugin from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // VITE_AUTH0_DOMAIN
 // VITE_AUTH0_CLIENT_ID
@@ -8,25 +8,25 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    const gatewayUrl = env.MARKETCUSTOMSGATEWAY_HTTPS
-        || env.services__marketCustomsGateway__https__0
-        || env.services__marketCustomsGateway__http__0
-        || env.MARKETCUSTOMSGATEWAY_HTTP
-        || 'http://localhost:5000';
+  const env = loadEnv(mode, process.cwd(), "");
+  const gatewayUrl =
+    env.MARKETCUSTOMSGATEWAY_HTTPS ||
+    env.services__marketCustomsGateway__https__0 ||
+    env.services__marketCustomsGateway__http__0 ||
+    env.MARKETCUSTOMSGATEWAY_HTTP ||
+    "http://localhost:5000";
 
-
-    return {
-        plugins: [plugin(), tailwindcss()],
-        server: {
-            port: Number(env.PORT) || 55577,
-            proxy: {
-                '/api': {
-                    target: gatewayUrl,
-                    changeOrigin: true,
-                    secure: false,
-                },
-            },
+  return {
+    plugins: [plugin(), tailwindcss()],
+    server: {
+      port: Number(env.PORT) || 55577,
+      proxy: {
+        "/api": {
+          target: gatewayUrl,
+          changeOrigin: true,
+          secure: false,
         },
-    };
+      },
+    },
+  };
 });
